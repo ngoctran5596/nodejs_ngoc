2725237eed6e00d4e5befd039ee7e0468c6fb6cb
DESKTOP-E39AOD8\PhuongQuang <dquang0706@gmail.com>
2021-06-05 17:37:16 +0700

    Complete CRUD course api feature

--- src/app/api/Course/controller.js
+++ src/app/api/Course/controller.js
@@ -1,11 +1,29 @@
 var Courses = require("../../models/Courses")
 
 exports.getAll = async function (req, res) {
-  await Courses.find({})
-    .lean()
-    .exec((err, data) => {
-      res.send(data)
+  const payload = await Courses.find()
+  res.status(200).json({
+    payload,
+  })
+}
+
+exports.getById = async function (req, res) {
+  try {
+    const payload = await Courses.findById(req.params.id)
+
+    if (!payload) {
+      res.status(404).json({
+        error: "Resource not found!!",
+      })
+    }
+    res.status(200).json({
+      payload,
     })
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    })
+  }
 }
 
 exports.store = async function (req, res) {
@@ -24,4 +42,45 @@ exports.store = async function (req, res) {
   }
 }
 
-// exports.insert = courseController.store();
+exports.update = async function (req, res) {
+  try {
+    const payload = await Courses.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!payload) {
+      res.status(404).json({
+        error: "Resource not found!!",
+      })
+    }
+    res.status(200).json({
+      payload,
+    })
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    })
+  }
+}
+
+exports.delete = async function (req, res) {
+  const payload = await Courses.findById(req.params.id)
+
+  if (!payload) {
+    res.status(404).json({
+      error: "Resource not found!!",
+    })
+  }
+
+  try {
+    await payload.delete()
+    res.status(200).json({
+      payload,
+    })
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    })
+  }
+}
--- src/app/api/Course/router.js
+++ src/app/api/Course/router.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const controller = require("./controller")
 
 router
-    .get("/", controller.getAll)
-    .post("/", controller.store)
+  .get("/", controller.getAll)
+  .get("/:id", controller.getById)
+  .post("/", controller.store)
+  .put("/:id", controller.update)
+  .delete("/:id", controller.delete)
 module.exports = router //exporst qua index.js để su dung
