bfa72e99f2d0b94bba639c04fc10cc57e2cc0a99
Tran Minh Ngoc <tmntranminhngoc@gmail.com>
2021-08-12 20:37:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,128 +1,125 @@
-require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const path = require("path");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const cors = require("cors");
-const exphbs = require("express-handlebars");
-const conectIo = require("./app/controllers/socket");
-const postController = require('./app/controllers/PostController');
-const auth = require('./middlewares/auth');
-var methodOverride = require("method-override");
-const User = require("./app/models/User");
-const {addUser,loadMessage,saveMsg} = require("./app/helpers/mics");
-const Messages = require("./app/models/Message");
-const app = express();
+require ('dotenv').config ();
+const express = require ('express');
+const http = require ('http');
+const path = require ('path');
+const morgan = require ('morgan');
+const cookieParser = require ('cookie-parser');
+const fileUpload = require ('express-fileupload');
+const cors = require ('cors');
+const exphbs = require ('express-handlebars');
+const conectIo = require ('./app/controllers/socket');
+const postController = require ('./app/controllers/PostController');
+const auth = require ('./middlewares/auth');
+var methodOverride = require ('method-override');
+const User = require ('./app/models/User');
+const {addUser, loadMessage, saveMsg} = require ('./app/helpers/mics');
+const Messages = require ('./app/models/Message');
+const app = express ();
 
 const port = 3000;
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
+var bodyParser = require ('body-parser');
+app.use (bodyParser.urlencoded ({extended: false}));
 
-const route = require("./routes");
+const route = require ('./routes');
 // var upload = multer({ dest: 'uploads/' })
-var multer  = require('multer');
-var storage = multer.diskStorage({
+var multer = require ('multer');
+var storage = multer.diskStorage ({
   destination: function (req, file, cb) {
-    cb(null, 'src/public/imageNewFeed')
+    cb (null, 'src/public/imageNewFeed');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now()  + "-" + file.originalname)
-  }
-}); 
-var upload = multer({ storage: storage})
-app.post('/postImage', upload.single('imagePost'), postController.create)
-//import db
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-app.use(cors());
-const io = new Server(server, {
-  cors: {
-    origin: "*",
+    cb (null, Date.now () + '-' + file.originalname);
   },
 });
-const db = require("./config/db");
-app.use(cookieParser());
-app.use(
-  fileUpload({
+var upload = multer ({storage: storage});
+app.post ('/postImage', upload.single ('imagePost'), postController.create);
+//import db
+const server = http.createServer (app);
+const {Server} = require ('socket.io');
+app.use (cors ());
+const io = new Server (server, {
+  cors: {
+    origin: '*',
+  },
+});
+const db = require ('./config/db');
+app.use (cookieParser ());
+app.use (
+  fileUpload ({
     useTempFiles: true,
   })
 );
 //connnect to db
-db.connect();
-app.use(methodOverride("_method"));
+db.connect ();
+app.use (methodOverride ('_method'));
 //public nhung file public
-app.use(express.static(path.join("src", "public")));
-app.use(express.static(path.join("src", "resources/assets")));
+app.use (express.static (path.join ('src', 'public')));
+app.use (express.static (path.join ('src', 'resources/assets')));
 // console.log(__dirname,'public');
 //http loger
 // app.use (morgan ());
-app.use(
-  express.urlencoded({
+app.use (
+  express.urlencoded ({
     extended: true,
   })
 );
-app.use(express.json());
+app.use (express.json ());
 
 // conectIo.conect(io);
-io.on("connection", async (socket) => {
-  console.log('user connect');
+io.on ('connection', async socket => {
+  console.log ('user connect');
   // get user
-  socket.on("getUsers", async () => {
-    User.find({},(err, users) => {
-      io.emit("getAllUser", users);
-    }).select('-password');
+  socket.on ('getUsers', async () => {
+    User.find ({}, (err, users) => {
+      io.emit ('getAllUser', users);
+    }).select ('-password');
 
-  
-    loadMessage(socket);
+    loadMessage (socket);
 
-    socket.on(
-      "startUniqueChat",
-      ({recieverEmail,senderEmail,recieverID},callback)=>{
-        addUser({recieverEmail,senderEmail,recieverID},socket);
+    socket.on (
+      'startUniqueChat',
+      ({recieverEmail, senderEmail, recieverID}, callback) => {
+        addUser ({recieverEmail, senderEmail, recieverID}, socket);
       }
     );
 
-    socket.on('joinTwoUsers',({roomID},cb)=>{
-      socket.join(roomID);
-      cb({roomID});
+    socket.on ('joinTwoUsers', ({roomID}, cb) => {
+      socket.join (roomID);
+      cb ({roomID});
     });
-
-    
-  
   });
-  socket.on('sendTouser',(data)=>{
-    console.log('DATATAT',data);
-    socket.broadcast.to(data.roomID).emit('dispatchMsg',{...data});
+
+  socket.on ('sendTouser', data => {
+    console.log ('DATATAT', data);
+    socket.broadcast.to (data.roomID).emit ('dispatchMsg', {...data});
     const {
       roomID,
       senderEmail,
       recieverEmail,
-      composeMsg :{ time, txtMsg},
+      composeMsg: {time, txtMsg},
     } = data;
 
-     new Messages({roomID,senderEmail,recieverEmail,time,txtMsg}).save();
+    new Messages ({roomID, senderEmail, recieverEmail, time, txtMsg}).save ();
   });
 });
 //template engine
-app.engine(
-  "hbs",
-  exphbs({
-    extname: ".hbs",
+app.engine (
+  'hbs',
+  exphbs ({
+    extname: '.hbs',
     //sử dụng function trong express handlerbar
     helpers: {
       sum: (a, b) => a + b,
-      dateFormat: require("handlebars-dateformat"),
+      dateFormat: require ('handlebars-dateformat'),
     },
   })
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set ('view engine', 'hbs');
+app.set ('views', path.join (__dirname, 'resources', 'views'));
 // console.log(path.join(__dirname,'resources/views'));
 //routes khoi tao tuyen duong
-route(app);
+route (app);
 
-server.listen(port, () => {
-  console.log(`http://localhost:3000`)
+server.listen (port, () => {
+  console.log (`http://localhost:3000`);
 });
